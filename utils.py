d3cc9d7d4ad34be80c63e5cffc349b95e9b83869
Omkar Pathak <omkarpathak27@gmail.com>
2018-12-15 07:51:06 +0530

    Added new resume and bug fix for plus sign in mobile number extraction

--- utils.py
+++ utils.py
@@ -59,7 +59,11 @@ def extract_mobile_number(text):
     # Found this complicated regex on : https://zapier.com/blog/extract-links-email-phone-regex/
     phone = re.findall(re.compile(r'(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'), text)
     if phone:
-        return ''.join(phone[0])
+        number = ''.join(phone[0])
+        if len(number) > 10:
+            return '+' + number
+        else:
+            return number
 
 def extract_skills(nlp_text):
     tokens = [token.text for token in nlp_text if not token.is_stop]
