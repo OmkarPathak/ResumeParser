e59410d55a1563188ef015f1db486a07ea39d024
Omkar Pathak <omkarpathak27@gmail.com>
2019-03-01 20:06:26 +0530

    Added more nltk dependencies

--- pre_requisites.py
+++ pre_requisites.py
@@ -7,3 +7,7 @@ os.system('python -m spacy download en_core_web_sm')
 # Install nltk Dependencies
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
