b360a90c8fcbbd5e9535c200dfb429cf79e5d2ba
Omkar Pathak <omkarpathak@Omkars-MacBook-Pro.local>
2025-12-21 14:41:54 +0530

    fix: NLTK data download path

--- build.sh
+++ build.sh
@@ -7,9 +7,10 @@ pip install -r resume_parser/requirements.txt
 pip install ./pyresparser
 
 # Download NLTK data
-python -m nltk.downloader stopwords
-python -m nltk.downloader punkt
-python -m nltk.downloader wordnet
+# Download NLTK data to local directory
+python -m nltk.downloader stopwords -d nltk_data
+python -m nltk.downloader punkt -d nltk_data
+python -m nltk.downloader wordnet -d nltk_data
 
 # Collect static files
 python resume_parser/manage.py collectstatic --no-input
--- resume_parser/resume_parser/settings.py
+++ resume_parser/resume_parser/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Add NLTK data path
+import nltk
+nltk.data.path.append(os.path.join(BASE_DIR, 'nltk_data'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
